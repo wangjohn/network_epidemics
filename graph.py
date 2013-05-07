1c5f0222618bb737679379d7c9acb3546b6fb58e
wangjohn <wangjohn@mit.edu>
2013-05-07 18:47:50 -0400

    Added matrices classes.

--- graph.py
+++ graph.py
@@ -1,8 +1,18 @@
 class Graph:
-    def __init__(self, adjacency_matrix):
+    def __init__(self, adjacency_matrix, protection_matrix):
         self.adjacency_matrix = adjacency_matrix
+        self.protection_matrix = protection_matrix
 
-class AdjacencyMatrix:
+class ProtectionMatrix(Matrix):
+    def __init__(self, num_nodes, cost_function):
+        self.cost_function = cost_function
+        super(Matrix, self).__init__(num_nodes)
+
+class AdjacencyMatrix(Matrix):
+    def set_edge(self, node_1, node_2, value = 1):
+        super(Matrix, self).set_edge(node_1, node_2, value)
+
+class Matrix:
     def __init__(self, num_nodes):
         self.num_nodes = num_nodes
         self.matrix = [[0 for i in xrange(num_nodes)] for j in xrange(num_nodes)]
@@ -15,7 +25,7 @@ class AdjacencyMatrix:
         for i in xrange(len(neighbor_list)):
             self.matrix[i] = neighbor_list[i]
 
-    def set_edge(self, node_1, node_2, value = 1):
+    def set_edge(self, node_1, node_2, value):
         self.matrix[node_1][node_2] = value
         self.matrix[node_2][node_1] = value
 
