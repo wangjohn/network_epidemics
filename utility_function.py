fa070b126d31bb50ecb8db7b15d63e2d5a2f47ab
wangjohn <wangjohn@mit.edu>
2013-05-08 23:46:14 -0400

    Changing utility function and making a polynomial cost function.

--- utility_function.py
+++ utility_function.py
@@ -1,13 +1,31 @@
 
+# Utility function class. Default uses a polynomial cost function using the
+# default of the PolynomialCostFunction class.
 class UtilityFunction:
-    def __init__(self, cost_function, uninfected_value = 1):
-        self.cost_function = cost_function
+    def __init__(self, cost_function = None, uninfected_value = 1):
         self.uninfected_value = uninfected_value
+        self.cost_function = cost_function
 
-# Polynomial cost function. The default cost function is just c(q) = q^2.
+        if not self.cost_function:
+            self.cost_function = PolynomialCostFunction()
+
+    def calculate_utility(self, infection_probability, protection):
+        cost = self.cost_function.calculate_cost(protection)
+        return self.uninfected_value * infection_probability - cost
+
+# An abstract class for the cost function. All cost functions should implement
+# this base class, and should redefine the +calculate_cost+ method.
 class CostFunction:
     def __init__(self, function_type = "polynomial"):
         self.function_type = function_type
+
+    def calculate_cost(self, protection):
+        raise "Not implemented"
+
+# Polynomial cost function. The default cost function is just c(q) = q^2.
+class PolynomialCostFunction(CostFunction):
+    def __init__(self, function_type = "polynomial"):
+        super(PolynomialCostFunction, self).__init__(function_type)
         self.coefficients = [0, 0, 0.5]
 
     def set_coefficients(self, coefficients):
