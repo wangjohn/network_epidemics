79ad9114766b7986b531b41435f8c3f01138513a
wangjohn <wangjohn@mit.edu>
2013-05-09 23:19:01 -0400

    Minor changes

--- game.py
+++ game.py
@@ -2,10 +2,10 @@ import graph
 import random
 
 class Game:
-    def __init__(self, graph, utility_function, zombie):
-        self.network = graph
+    def __init__(self, graph, utility_function, start_node):
+        self.graph = graph
         self.utility_function = utility_function
-        self.zombie = zombie
+        self.start_node = start_node
         #0 is utility maximizing, 1 is social welfare maximizing
         self.node_types = [0 for n in range(graph.num_nodes)]
 
@@ -21,17 +21,18 @@ class Game:
 
     def setFTopDegree(self, f):
         degree_list = self.graph.degree_list
-        topDegreeIndices = sorted(range(len(degree_list)), key=lambda i: degree_list[i])[-floor(f*len(degree_list)):]
-        for index in topDegreeIndices:
+        top_degree_indices = sorted(range(len(degree_list)), key=lambda i: degree_list[i])[-floor(f*len(degree_list)):]
+        for index in top_degree_indices:
             self.node_types[index] = 1
 
     #assume everyone is of type 0 - selfish type
     def compute_nash(self):
-        securityProfile = [0 for i in range(len(self.network.num_nodes))]
-        frontier = [self.zombie]
-        visitedNodes = []
+        security_profile = [0 for i in range(len(self.graph.num_nodes))]
+        frontier = [self.start_node]
+        visited_nodes = []
         while frontier != []
             current_node = frontier.pop()
+
 
 
 class Agent:
