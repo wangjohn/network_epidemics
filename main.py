a27f2365e558eb70f0fa6090dd5bc0ff67d78f26
wangjohn <wangjohn@mit.edu>
2013-05-12 08:39:13 -0400

    Changing main around

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import infection
 import graph
 import erdos_renyi
 
-probability = 0.1
-num_nodes = 100
-num_trials = 50
-er_generator = erdos_renyi.ErdosRenyi(probability, num_nodes)
+def compute_average_probability(graph, protection_range, num_trials = 250):
+    probabilities_hash = {}
+    protection_string = "{:15s} {:15s} {:15s}"
+    print protection_string.format("Protection", "Probability", "Network Effect")
+    for q in protection_range:
+        protection_list = [q for i in xrange(graph.num_nodes)]
+        compute_probabilities_object = infection.ComputeInfectionProbabilities(graph, protection_list, 0)
 
-graph = graph.Graph(er_generator.draw_adjacency_matrix())
+        probabilities = compute_probabilities_object.monte_carlo_compute(num_trials)
+        probabilities_hash[q] = sum(probabilities) / num_nodes
+        protection_string.format(str(probabilities), str(probabilities_hash[q]), str(1 - probabilities - probabilities_hash[q]))
+    return probabilities_hash
 
-protection_list = [0.25 for i in xrange(num_nodes)]
-compute_probabilities_object = infection.ComputeInfectionProbabilities(graph, protection_list, 0)
 
-probabilities = compute_probabilities_object.monte_carlo_compute(num_trials)
-print "Average Monte Carlo Infection Probabilities", sum(probabilities) / num_nodes
+if __name__ == '__main__':
+    erdos_edge_probability = 0.1
+    num_nodes = 100
+    er_generator = erdos_renyi.ErdosRenyi(erdos_edge_probability, num_nodes)
+    graph = graph.Graph(er_generator.draw_adjacency_matrix())
+    protection_range = [float(i)/100 for i in xrange(5, 100, 5)]
+
+    print compute_average_probability(graph, protection_range)
