fd38466046fcc5a8f5f45fc7014b0040f0bf764e
wangjohn <wangjohn@mit.edu>
2013-05-12 07:04:56 -0400

    Changed the ER generator to make more sense.

--- erdos_renyi.py
+++ erdos_renyi.py
@@ -19,13 +19,10 @@ class ErdosRenyi:
         self.num_nodes = num_nodes
 
     def draw_adjacency_matrix(self):
-        adjacency_matrix = AdjacencyMatrix(self.num_nodes)
-        return self._set_matrix_edges(adjacency_matrix)
-
-    # Mutates the adjacency matrix and sets the matrix according to the
-    # probability of set by Erdos Renyi.
-    def _set_matrix_edges(self, adjacency_matrix):
+        adjacency_matrix = graph.AdjacencyMatrix(self.num_nodes)
         for i in xrange(num_nodes):
             for j in xrange(num_nodes):
                 if i <= j and random.random() < self.p:
                     adjacency_matrix.set_edge(i, j)
+
+        return adjacency_matrix
